72f97da97d17619bb3c3230ddfc886f26ac254d4
AntonVoronezh <pstut@mail.ru>
2019-01-14 20:24:59 +0300

    makeMarking

--- cross-zero/cross-zero.js
+++ cross-zero/cross-zero.js
@@ -19,6 +19,23 @@ function startCrossZero() {
     let flag = true;
 
 
+    function makeMarking() {
+
+        getElem.innerHTML = '<h1>Крестики-нолики </h1>\
+            aka AntonVoronezh\
+             <br> <br> <br>\
+            <table class="field"></table>\
+            <p>Победитель: <span class="winner">?</span></p>\
+            <p>Сейчас ходит: <span class="gamer">X</span></p>\
+            <button class="refresh">Начать заново</button>\
+            ';
+
+        getElem.style.border = '1px solid #000';
+        getElem.style.textAlign = 'center';
+        getElem.style.padding = '20px';
+
+    };
+
 
 
  
