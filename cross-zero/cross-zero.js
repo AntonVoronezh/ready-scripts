be5b2a7fdb2f46b62b34d4fab785574e63c0fe2c
AntonVoronezh <pstut@mail.ru>
2019-01-14 20:24:42 +0300

    setup

--- cross-zero/cross-zero.js
+++ cross-zero/cross-zero.js
@@ -3,6 +3,20 @@ function startCrossZero() {
     // <div id="cross-zero"></div>
     // startCrossZero();
             
+    const getElem = document.getElementById('cross-zero');
+
+    makeMarking();
+
+    const getTbl = document.querySelector('.field');
+    const getBtn = document.querySelector('.refresh');
+    const getGamer = document.querySelector('.gamer');
+    const getWinner = document.querySelector('.winner');
+
+    makeClearField();
+    makeRefresh();
+    makeProgress();
+
+    let flag = true;
 
 
 
