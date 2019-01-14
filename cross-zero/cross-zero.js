04c574c7e02d3ac69ac42e7d6aec7015679ea0a7
AntonVoronezh <pstut@mail.ru>
2019-01-14 20:26:38 +0300

    winCombinations

--- cross-zero/cross-zero.js
+++ cross-zero/cross-zero.js
@@ -76,6 +76,19 @@ function startCrossZero() {
 
         const $ = getTbl.querySelectorAll('.ceil');
 
+        const winCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+
+            [0, 5, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+
+            [0, 4, 8],
+            [6, 4, 2]
+        ];
+
 
 
 
