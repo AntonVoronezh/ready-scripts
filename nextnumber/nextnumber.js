94b2715e0295d6814c7b67ea2df5d49e34653e77
AntonVoronezh <pstut@mail.ru>
2019-01-15 18:53:23 +0300

    init

---
+++ nextnumber/nextnumber.js
@@ -0,0 +1,38 @@
+function nextNumber() {
+
+    // <script src="nextnumber.js"></script>
+    // <div id="next-number">
+
+    const getElem = document.getElementById('next-number');
+
+    let round = 2;
+
+    init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
+
