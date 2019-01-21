1fb9bc5ef1fa58058276a392ac7b2950a8ffd64d
AntonVoronezh <pstut@mail.ru>
2019-01-21 10:16:47 +0300

    makeCalendar

---
+++ calendar/calendar.js
@@ -0,0 +1,39 @@
+function makeCalendar() {
+
+    // <div id="calendar"></div>
+    // makeCalendar();
+
+    const calendar = document.querySelector('#calendar');
+
+    setStyle();
+    makeMarking();
+
+    const calendarInfo = calendar.querySelector('.calendar__info');
+    const calendarDates = calendar.querySelector('.calendar__dates');
+    const calendarPrev = calendar.querySelector('.calendar__prev');
+    const calendarNext = calendar.querySelector('.calendar__next');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
