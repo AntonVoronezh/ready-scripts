57d8627399fddf71f8ce3d50c7c4fedd9d603f96
AntonVoronezh <pstut@mail.ru>
2019-01-25 18:43:15 +0300

    init

---
+++ calculatearea/calculatearea.js
@@ -0,0 +1,58 @@
+(function calculateArea() {
+    // <div id="area"></div>
+
+    const area = document.querySelector('#area');
+
+    makeMarking();
+
+    setStyle();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
+
+
+
+
+
