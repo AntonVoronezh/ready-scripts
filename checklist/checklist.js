0c7d4db7cc4be2196ae086368b8c2fb331b1f1d2
AntonVoronezh <pstut@mail.ru>
2019-01-26 14:44:52 +0300

    addNewNote

--- checklist/checklist.js
+++ checklist/checklist.js
@@ -21,7 +21,36 @@
     });
 
 
-   
+    function addNewNote(argText) {
+
+        const createLi = createElem('li');
+        const createCheck = createElem('input');
+        const createClose = createElem('div');
+        const createText = createElem('div');
+        const createBackInp = createElem('input');
+
+        addClass(createLi, 'checklist__li');
+        addClass(createText, 'checklist__text');
+        addClass(createCheck, 'checklist__ckeckbox');
+        addClass(createClose, 'checklist__close');
+        addClass(createBackInp, 'checklist__none');
+        addClass(createBackInp, 'checklist__back-input');
+
+        createLi.dataset.number = getCountNotes();
+        createText.dataset.number = getCountNotes();
+        createCheck.dataset.number = getCountNotes();
+        createClose.dataset.number = getCountNotes();
+        createBackInp.dataset.number = getCountNotes();
+
+
+
+
+
+
+
+
+    };
+
    
 
     
