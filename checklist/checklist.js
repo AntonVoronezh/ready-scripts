2b5eaffecf75afde18d3db0487647b4688b08f4e
AntonVoronezh <pstut@mail.ru>
2019-01-26 14:44:19 +0300

    init

---
+++ checklist/checklist.js
@@ -0,0 +1,31 @@
+(function checklist() {
+
+    const getElem = document.getElementById('checklist');
+
+    init();
+
+    const getInput = getElem.querySelector('.checklist__input');
+    const getUl = getElem.querySelector('.checklist__ul');
+
+
+
+
+   
+   
+
+    
+   
+
+    
+
+   
+
+   
+
+
+
+
+
+  
+
+}());
