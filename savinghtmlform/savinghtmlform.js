ea87324466916b9051dc6d68b8cb9e0c91610807
AntonVoronezh <pstut@mail.ru>
2019-01-31 15:20:58 +0300

    init

---
+++ savinghtmlform/savinghtmlform.js
@@ -0,0 +1,66 @@
+(function savinghtmlform() {
+
+    // <div id="savinghtmlform" class="saving-html-form"></div>
+
+    const savingHtmlForm = document.querySelector('#savinghtmlform');
+
+
+    makeMarking();
+
+    setStyle();
+
+
+    const form = savingHtmlForm.querySelector('form');
+
+    let input = form.querySelectorAll('input[type=text]');
+
+    let textarea = form.querySelectorAll('textarea');
+
+    let checkbox = form.querySelectorAll('input[type=checkbox]');
+
+    let radio = form.querySelectorAll('input[type=radio]');
+
+    let select = form.querySelectorAll('select');
+
+    const formWrap = document.querySelector('.saving-html-form__form');
+
+    const inputBtn = document.querySelector('.saving-html-form__input');
+
+    const textareaBtn = document.querySelector('.saving-html-form__textarea');
+
+    const checkboxBtn = document.querySelector('.saving-html-form__checkbox');
+
+    const radioBtn = document.querySelector('.saving-html-form__radio');
+
+    const selectBtn = document.querySelector('.saving-html-form__select');
+
+    const inputSelect = document.querySelector('.saving-html-form__input-select');
+
+    const resetBtn = document.querySelector('.saving-html-form__delete');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+    
+
+
+}());
