bf9334a0fdda83dc1f2a27ee450260ebc3f25557
AntonVoronezh <pstut@mail.ru>
2019-02-03 18:45:21 +0300

    start

--- numberfor7steps/numberfor7steps.js
+++ numberfor7steps/numberfor7steps.js
@@ -6,6 +6,18 @@
 
     setStyle();
 
+    const start = num7st.querySelector('.num7st__start');
+
+    const count = num7st.querySelector('.num7st__count');
+
+    const wrap = num7st.querySelector('.num7st__wrap');
+
+    const number = num7st.querySelector('.num7st__number');
+
+    const less = num7st.querySelector('.num7st__less');
+
+    const more = num7st.querySelector('.num7st__more');
+    
 
 ;
 
