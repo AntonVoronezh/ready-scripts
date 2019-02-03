7cbb86f8c4833e6530f307a265f5b37b10c18637
AntonVoronezh <pstut@mail.ru>
2019-02-03 18:45:06 +0300

    setup

---
+++ numberfor7steps/numberfor7steps.js
@@ -0,0 +1,32 @@
+(function numberfor7steps() {
+
+    const num7st = document.querySelector('#num7st');
+
+    makeMarking();
+
+    setStyle();
+
+
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}());
