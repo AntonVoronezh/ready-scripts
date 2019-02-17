08cc58a1a4fa5b3a2c580b528e87aeddfd325fff
AntonVoronezh <pstut@mail.ru>
2019-02-17 20:42:12 +0300

    makeMarking

--- life/life.js
+++ life/life.js
@@ -4,4 +4,20 @@
 
     const start = life.querySelector('.life__start');
 
+    makeMarking();
+
+    function makeMarking() {
+
+        life.innerHTML = `
+        <div class="life__wrap">
+        <input type="text" class="life__input life__width" placeholder="ширина (кол-во клеток)">
+        <input type="text" class="life__input life__height" placeholder="высота (кол-во клеток)">
+        Если просто нажать Старт, <br> то будет поле 20х20 клеток
+        <button class="life__start">старт</button>
+        </div>
+        <div class="life__field"></div>
+            `;
+
+    };
+
 })();
