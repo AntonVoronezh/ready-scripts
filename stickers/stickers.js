d6d321b2f25b95d4c7ddc3b8fcf05f8025da4812
AntonVoronezh <pstut@mail.ru>
2019-02-25 17:22:57 +0300

    MapWork

--- stickers/stickers.js
+++ stickers/stickers.js
@@ -50,6 +50,53 @@
 
     }
 
+    class MapWork {
+
+        constructor(argObj) {
+
+            this._map = argObj;
+
+        }
+
+        console() {
+
+            // console.log('_map', this._map);
+            // console.log('_map.size', this.size());
+
+        }
+
+        add(argStick) {
+
+            this._map.set(this._num(), argStick);
+
+            this.console();
+
+        }
+
+        size() {
+
+            return this._num();
+
+        }
+
+        _num() {
+
+            return this._map.size;
+
+        }
+
+        _for() {
+
+            this._map.forEach((elem, i) => {
+
+                console.log('_for', i, elem);
+
+
+            });
+
+        }
+
+    }
 
 
 
